13ebafe83ec41eaf576f124c211f4097c6d10af6
pdruart <pdruart@student.codam.nl>
2020-11-15 20:42:53 +0100

    it all works, but it's not norm compliant in that there are 44 lines in ft_split instead of the limit of 25

--- ft_split.c
+++ ft_split.c
@@ -6,39 +6,74 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/13 14:56:02 by pdruart       #+#    #+#                 */
-/*   Updated: 2020/11/14 18:04:14 by pdruart       ########   odam.nl         */
+/*   Updated: 2020/11/15 20:32:02 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_str_count(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+	int		start;
+
+	i = 0;
+	count = 0;
+	start = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) != c && (i == 0 || *(s + i - 1) == c))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
-	size_t	start;
+	long	start;
+	char	**arr;
 	char	*ptr;
-//found out how to best malloc the array, just malloc the amount of times the delimiter occurs (without it being proceeded by another occurance of the delimiter) + 1 for the NULL pointer and then just put the pointers generated for every string into that array
+	size_t	num;
+
 	if (s == NULL)
 		return (NULL);
+	num = ft_str_count(s, c);
+	arr = (char **)malloc((num + 1) * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
 	i = 0;
-	start = 0;
-	ptr = NULL;
+	start = -1;
 	while (*(s + i) != '\0')
 	{
-		if (ptr == NULL && *(s + i) != c)
-		{
-			ptr = (char *)(s + i);
+		if (start == -1 && *(s + i) != c)
 			start = i;
-		}
-		else if (ptr != NULL && *(s + i) == c)
+		else if (start != -1 && *(s + i) == c)
 		{
-			if (start)
-				start = 2;
-			//don't work lmao finish it up once I figure out how to best malloc the array
-			ptr = NULL;
+			ptr = (char *)malloc(i - start + 1);
+			if (ptr == NULL)
+				return (NULL);
+			ft_memmove(ptr, (s + start), i - start);
+			*(ptr + i - start) = '\0';
+			*arr = ptr;
+			arr++;
+			start = -1;
 		}
 		i++;
 	}
-	return (NULL);
-	
+	if (start != -1)
+	{
+		ptr = (char *)malloc(i - start + 1);
+		if (ptr == NULL)
+			return (NULL);
+		ft_memmove(ptr, (s + start), i - start);
+		*(ptr + i - start) = '\0';
+		*arr = ptr;
+		arr++;
+		start = -1;
+	}
+	*arr = NULL;
+	return ((arr - num));
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/11 17:30:02 by pdruart       #+#    #+#                 */
-/*   Updated: 2020/11/14 17:53:04 by pdruart       ########   odam.nl         */
+/*   Updated: 2020/11/15 16:23:11 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,34 +29,21 @@ int		contains(char const *set, char const chr)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char			*str;
-	unsigned int	start;
+	char			*ptr;
 	unsigned int	end;
-	unsigned int	i;
 
-	start = 0;
 	if (s1 == NULL || set == NULL)
 		return (NULL);
-	while (*(s1 + start) != '\0')
-	{
-		if (contains(set, *(s1 + start)) == 0)
-			break ;
-		start++;
-	}
-	end = ft_strlen(s1) - 1;
-	while (end > start)
-	{
-		if (contains(set, *(s1 + end)) == 0)
-			break ;
+	str = (char *)s1;
+	while (*str != '\0' && contains(set, *str) != 0)
+		str++;
+	end = ft_strlen(str);
+	while (end > 0 && contains(set, *(str + end - 1)) != 0)
 		end--;
-	}
-	str = malloc(end + 1 - start);
-	i = 0;
-	while (start <= end)
-	{
-		*(str + i) = *(s1 + start);
-		start++;
-		i++;
-	}
-	*(str + i) = '\0';
-	return (str);
+	ptr = malloc(end + 1);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memmove(ptr, str, end);
+	*(ptr + end) = '\0';
+	return (ptr);
 }
