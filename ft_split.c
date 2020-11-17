d7479cd2f3a4b00bfd9235414560cc62d505d3a6
pdruart <pdruart@student.codam.nl>
2020-11-17 19:20:28 +0100

    I think it works but still requires testing on a mac at codam to make sure theres no weirdness with linux

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/13 14:56:02 by pdruart       #+#    #+#                 */
-/*   Updated: 2020/11/15 20:32:02 by pdruart       ########   odam.nl         */
+/*   Updated: 2020/11/17 16:28:49 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ size_t	ft_str_count(char const *s, char c)
 {
 	size_t	i;
 	size_t	count;
-	int		start;
 
 	i = 0;
 	count = 0;
-	start = 0;
 	while (*(s + i) != '\0')
 	{
 		if (*(s + i) != c && (i == 0 || *(s + i - 1) == c))
@@ -30,12 +28,26 @@ size_t	ft_str_count(char const *s, char c)
 	return (count);
 }
 
+int		doit(char const *s, size_t i, long *start, char ***arr)
+{
+	char	*ptr;
+
+	ptr = (char *)malloc(i - *start + 1);
+	if (ptr == NULL)
+		return (1);
+	ft_memmove(ptr, (s + *start), i - *start);
+	*(ptr + i - *start) = '\0';
+	**arr = ptr;
+	*arr = *arr + 1;
+	*start = -1;
+	return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	long	start;
 	char	**arr;
-	char	*ptr;
 	size_t	num;
 
 	if (s == NULL)
@@ -50,30 +62,12 @@ char	**ft_split(char const *s, char c)
 	{
 		if (start == -1 && *(s + i) != c)
 			start = i;
-		else if (start != -1 && *(s + i) == c)
-		{
-			ptr = (char *)malloc(i - start + 1);
-			if (ptr == NULL)
-				return (NULL);
-			ft_memmove(ptr, (s + start), i - start);
-			*(ptr + i - start) = '\0';
-			*arr = ptr;
-			arr++;
-			start = -1;
-		}
+		else if (start != -1 && *(s + i) == c && doit(s, i, &start, &arr) == 1)
+			return (NULL);
 		i++;
 	}
-	if (start != -1)
-	{
-		ptr = (char *)malloc(i - start + 1);
-		if (ptr == NULL)
-			return (NULL);
-		ft_memmove(ptr, (s + start), i - start);
-		*(ptr + i - start) = '\0';
-		*arr = ptr;
-		arr++;
-		start = -1;
-	}
+	if (start != -1 && doit(s, i, &start, &arr) == 1)
+		return (NULL);
 	*arr = NULL;
 	return ((arr - num));
 }
