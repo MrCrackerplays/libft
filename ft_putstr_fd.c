ce38e9b8e98c705706af264386f9b05d9998dd96
pdruart <pdruart@student.codam.nl>
2020-11-22 11:43:42 +0100

    optimized ft_substr's memory usage by changing len where needed and optimized ft_putstr_fd by only calling write once and using the string size instead

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,17 +6,14 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/11 15:18:55 by pdruart       #+#    #+#                 */
-/*   Updated: 2020/11/14 15:28:23 by pdruart       ########   odam.nl         */
+/*   Updated: 2020/11/22 11:41:06 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "unistd.h"
+#include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	while (s != NULL && *s != '\0')
-	{
-		write(fd, s, 1);
-		s++;
-	}
+	write(fd, s, ft_strlen(s));
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/11 14:48:53 by pdruart       #+#    #+#                 */
-/*   Updated: 2020/11/14 18:13:47 by pdruart       ########   odam.nl         */
+/*   Updated: 2020/11/22 11:36:16 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
+	size_t	slen;
 
+	if (s == NULL)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		len = 0;
+	else if (start + len > slen)
+		len = slen - start;
 	str = (char *)malloc(len + 1);
-	if (s == NULL || str == NULL)
+	if (str == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len && start < ft_strlen(s))
+	while (i < len)
 	{
 		*(str + i) = *(s + start + i);
 		i++;
