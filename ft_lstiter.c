c0a8ecc3c8e04ee33b9b5a93986a9fe1c78305b3
pdruart <pdruart@student.codam.nl>
2020-11-25 11:44:33 +0100

    changed lstiter to iterate from start to back instead of from back to front, doesn't matter in most cases but it's more intuitive in case that the order does matter

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/19 13:54:42 by pdruart       #+#    #+#                 */
-/*   Updated: 2020/11/19 13:58:31 by pdruart       ########   odam.nl         */
+/*   Updated: 2020/11/25 11:41:08 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (lst == NULL)
 		return ;
-	ft_lstiter(lst->next, f);
 	(*f)(lst->content);
+	ft_lstiter(lst->next, f);
 }
