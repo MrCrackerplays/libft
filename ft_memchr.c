22cefceaa07d4c15e2a1f67a8dade60e08759bc5
pdruart <pdruart@student.codam.nl>
2020-11-29 10:52:15 +0100

    possibly fixed moulinette issue with memchr? char -> unsigned char

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,14 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*chrs;
+	size_t			i;
+	unsigned char	*chrs;
 
-	chrs = (char *)s;
+	chrs = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		if (*chrs == c)
+		if (*chrs == (unsigned char)c)
 			return (chrs);
 		chrs++;
 		i++;
